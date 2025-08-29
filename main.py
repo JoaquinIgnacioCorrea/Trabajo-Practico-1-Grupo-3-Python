2c749f310627c026e763a4c9ba7afefc095384c7
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2025-08-29 20:42:19 -0300

    agregado limpiado de consola al menu

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 
+def clean_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 def menu():
     menu = True
     games = obtain_games()
     while menu:
+        clean_console()
         print("\n--- Menú ---")
         for i, game in enumerate(games, 1):
             print(f"{i}. Jugar {(game.replace('_', ' ').title()).replace('.Py', '')}")
@@ -11,21 +18,22 @@ def menu():
         opcion = input("Selecciona una opción: ")
         
         if opcion in [str(i) for i in range(1, len(games) + 1)]:
+            clean_console()
             os.system(f"python {games[int(opcion) - 1]}")
+            input("\nPresiona Enter para volver al menú...")
         elif opcion == str(len(games) + 1):
+            clean_console()
             print("Saliendo...")
             menu = False
         else:
             print("Opción inválida. Intenta de nuevo.")
+            input("Presiona Enter para continuar...")  # pausa para que vea el error
 
 def obtain_games():
     obtained_games = []
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    return [
-        "piedra_papel_o_tijera.py",
-        "quiz_python.py",
-        "ta_te_ti.py",
-        "adivinar_el_numero.py"
-    ]
+    for file in os.listdir("."):
+        if file.endswith(".py") and file != "main.py":
+            obtained_games.append(file)
+    return obtained_games
 
 menu()
